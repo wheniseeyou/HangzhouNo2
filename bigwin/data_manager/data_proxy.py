8be4bf7ad39774f49cc30c8db27277a78fe427b3
姚天舒 <yaotianshu@yaotianshudeMacBook-Pro.local>
2020-12-13 12:06:06 +0800

    和马俊杰开会商量好股票交易规则 并做详细注释

--- bigwin/data_manager/data_proxy.py
+++ bigwin/data_manager/data_proxy.py
@@ -7,6 +7,7 @@
 3， 按财务周期
 """
 import yaml
+import logging
 import os
 
 FILE_PATH = os.path.dirname(__file__)
@@ -15,15 +16,24 @@ CONFIG_PATH = "/".join(FILE_PATH.split("/")[:-1])   # only get up root
 class DataProxy(object):
     CONFIG_YAML_PATH = CONFIG_PATH + "/config.yaml"
     def __init__(self, order_id: str, start_date: str, end_date: str, frequency_type: str):
-        pass
+        all_frequency_type_list = self._get_frequency_list(self.CONFIG_YAML_PATH)
+        if frequency_type not in all_frequency_type_list:
+            logging.error("你输入的参数有误，请检查你输入的频率参数")
+        
 
         
     
     def _get_config_parse(self, file_path: str):
         f = open(file_path, "r", encoding="utf-8")
         content_text = f.read()
-        yaml_reader = yaml.load(content_text)
+        yaml_reader = yaml.load(stream=content_text, Loader=yaml.FullLoader)
         return yaml_reader
+
+    def _get_frequency_list(self, file_path: str):
+        all_config_tasker = self._get_config_parse(file_path)
+        return all_config_tasker.get("TRANSCATION").get("FREQUENCY").split(",")
+    
+
 
         
         
